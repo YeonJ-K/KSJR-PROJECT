b603beb18595478417646461bb0a4b5ecb664f2f
jw0113 <43206047+jw0113@users.noreply.github.com>
2021-05-26 08:26:32 +0900

    Update fileopen.py

--- fileopen.py
+++ fileopen.py
@@ -1,6 +1,24 @@
 #사용자가 입력해야 하는 명령어 양식
 #python fileopen.py 경로+파일명
+#tip. flask 값 넘기는 방법
+#서버 필요?
 import sys
+
+html_text1 = """
+    <!DOCYTPE html>
+    <html>
+    <head>
+    <title>TEST Page</title>
+    </head>
+    <body>
+    <p>
+    """
+
+html_text2 = """
+    </p>
+    </body>
+    </html>
+    """
 
 #파일 열기
 f = open(sys.argv[1])
@@ -9,6 +27,13 @@ f = open(sys.argv[1])
 r = f.read()
 print(r)
 
+#html 파일에 추가하기
+with open('html_file.html', 'a') as html_file:
+    html_file.write(html_text1 + r + html_text2)
+
 f.close()
+
+
+
 
 
