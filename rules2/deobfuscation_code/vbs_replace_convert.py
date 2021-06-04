10c9604c1a9494dd21f21ed4b2b4a32f2ba7945b
YeonJ-K <59119195+YeonJ-K@users.noreply.github.com>
2021-06-04 18:56:52 +0900

    Update vbs_replace_convert.py

--- rules2/deobfuscation_code/vbs_replace_convert.py
+++ rules2/deobfuscation_code/vbs_replace_convert.py
@@ -1,7 +1,8 @@
 import re
-str = ''
-a = re.compile("^(E|e)[\\w]{1,12}[\\S][\\w]{1,}", re.I)
-find = a.match(str)
+str = 'ExecutEglobAl(Tr) :'
+a = re.compile("(?=(E|e)[\w]{1,12}[\S][\w]{1,}[\W])(?=(E|e)[\w]{3,}[\S]*[\s]{1,}).*")
+find = re.match(a, str)
 if find:
-    a = (find.group()).lower().replace("execute", "write")
+    a = (find.group()).lower()
+    a= a.replace('executeglobal','Wscript.Echo')
 print(a)
