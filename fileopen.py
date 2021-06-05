71d85fa58b8a4a4ab86a99c5ee986eb3093ee3cb
Gwangun Jung <exsociety@gmail.com>
2021-06-05 11:52:34 +0000

    temp

--- fileopen.py
+++ fileopen.py
@@ -6,6 +6,7 @@ import sys
 import re
 import json
 import glob
+import argparse
 from rules import *
 
 
@@ -25,35 +26,26 @@ html_text2 = """
     </html>
     """
 
-#파일 열기
-f = open(sys.argv[1])
 
-#파일 전체 읽기
-r = f.read()
-#print(r)
-index=1
+
+'''
 
 #Rule 읽어오는 파일
-path = glob.glob(r'rules/*.json')
+rule_files = glob.glob(r'rules/*.json')
 rules = [] # 
 key_list = []
 title = []
 descriptions = []
 deobfuscation = []
 code_convert = []
-for a in path:
-    json_data = json.loads(open(a).read()) 
-    rules.append(json_data)
-    key_list.append(json_data['regexp'])
-# print(json.dumps(key_list, indent='\t'))
-# print(json.dumps(rules, indent='\t'))  # 제대로 읽어오는지 test용
+
 
 #문자열을 전부 읽어와서 for문 돌리기 (전부 읽어오기와 한줄씩 읽어오기가 동시에 불가)
 for i in r.split("\n"):
     #index는 나중에 p태그의 id값으로 사용될 예정, 지금은 예시로 출력만
     #i는 받아온 파싱 내용 중에서 한줄씩 들어감
     print('%d %s' %(index, i))
-    for a in path:
+    for a in rule_files:
         json_data = json.loads(open(a).read()) 
         key = json_data['regexp']
         detect = re.compile(key)
@@ -68,3 +60,79 @@ with open('html_file.html', 'a') as html_file:
     html_file.write(html_text1 + r + html_text2)
 
 f.close()
+
+'''
+
+
+
+def  load_rules(rule_path:str) -> list:
+
+    rules = []
+
+    rule_pattern = f'{rule_path}/*.json'
+    rule_files = glob.glob(rule_pattern)
+
+    for rule_file in rule_files:
+        try:
+            json_data = json.loads(open(rule_file).read()) 
+            rules.append(json_data)
+        except Exception as e:
+            pass
+    return rules
+
+def load_malware(path:str) -> list:
+    try:
+        lines = open(path).readlines()
+        return lines
+    except:
+        return []
+
+def match_rule(lines:list, rules:list) -> dict:
+
+
+    for line_idx in range(len(lines)):
+        line_res = {}
+
+        for rule in rules:
+            print(f'{line_idx} match {rule["title"]}')
+
+            pattern = rule['regexp']
+            print(pattern)
+            try:
+                regexp = re.compile(pattern)
+            except Exception as e:
+                print(f'error {rule["title"]}')
+            #matched_res = re.search(regexp, lines[line_idx])
+
+            #print(matched_res)
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Parse Script')
+    parser.add_argument('input', type=str, 
+                    help='input file')
+    
+    parser.add_argument('--rules', type=str, 
+                    help='rule path')
+
+    args = parser.parse_args()
+
+    rule_path = './rules'
+    if args.rules != None and  args.rules != '':
+        rule_path = args.rules
+
+    print('[+] Load Rules')
+    rules = load_rules(rule_path)
+    print(f'    [-] {len(rules)} rules loaded')
+
+    source_lines = load_malware(args.input)
+
+    match_rule(source_lines, rules)
+
+
+
+
+if __name__ == '__main__':
+    main()
