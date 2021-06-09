0ff97124fa5cf2405bb00c448b541364098eb67d
YeonJ-K <59119195+YeonJ-K@users.noreply.github.com>
2021-06-10 00:29:34 +0900

    코드 처리 완료

--- rules2/deobfuscation_code/base64_deobfuscation.py
+++ rules2/deobfuscation_code/base64_deobfuscation.py
@@ -1,8 +1,10 @@
+import sys
+import os
 import base64
-base64_str =  '' # 해당 변수에 파싱, 탐지한 Base64 코드 담는다.
-'''
-파싱한 부분을 담는 곳은
-주석처리해서 말씀해주세요
-'''
-base64_bytes = base64.b64decode(base64_str)
-base64_decode = base64_bytes.decode('ascii')
+
+
+def deobfus_code(deobfuscation) :
+    base64_str = deobfuscation
+    base64_bytes = base64.b64decode(base64_str)
+    result_deobfuscation = base64_bytes.decode('ascii')
+    print(f"deobfuscation_result : " + result_deobfuscation)
